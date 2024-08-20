1ea5f6e272c45a7f3270a7cd047eb1ed918fe4d5
DurgeshSinghQA <durgesh.singh100793@gmail.com>
2024-08-20 08:38:10 +0530

    Find duplucate number within array elements

---
+++ DurgeshJavaLearning/src/JavaLearningPackage/DuplicateNumbers.java
@@ -0,0 +1,43 @@
+package JavaLearningPackage;
+
+import java.util.Arrays;
+
+public class DuplicateNumbers {
+	
+	public static void main(String[] Args)
+	{
+		
+		
+		
+		int[] nbr = {12,43,2,4,2,6,0,-7, 43,-7, 2, 2};
+		int[] dup = new int[nbr.length];
+		
+			
+		int k=0;
+		for(int i=0; i<nbr.length; i++)
+		{
+			for(int j=i+1; j<nbr.length; j++)
+			{
+			
+				if(nbr[i]==nbr[j])
+				{
+					System.out.println(nbr[i]);
+					dup[k]= nbr[i];
+					k++;
+					
+				
+					
+					
+				}
+			
+			}
+				
+		}
+		
+		System.out.println("Duplicate Values in arrays"+Arrays.toString(dup));
+		
+	}
+	
+	
+
+}
