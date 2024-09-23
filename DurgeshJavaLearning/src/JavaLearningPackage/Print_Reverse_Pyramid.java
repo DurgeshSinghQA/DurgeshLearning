3163af6081b6070aa1fe9557b85aa86182f039b0
DurgeshSinghQA <durgesh.singh100793@gmail.com>
2024-09-23 11:44:23 +0530

    Print_Reverse_Pyramid_modified

--- DurgeshJavaLearning/src/JavaLearningPackage/Print_Reverse_Pyramid.java
+++ DurgeshJavaLearning/src/JavaLearningPackage/Print_Reverse_Pyramid.java
@@ -4,18 +4,17 @@ public class Print_Reverse_Pyramid
 {
 	public static void main(String[] args) 
 	{
-		int num=9, row=4, col=4;
-		
-		for(int i=0;i<row;i++)
+		int num=9;
+		for(int i=4;i>=1;i--)
 		{
-			for(int j=0;j<col;j++)
+			for(int j=i;j>=1;j--)
 			{
 				System.out.print(num);
-				--num;
+				num--;
 			}
-			col--;
-			System.out.println();
 			
+			System.out.println();
+		
 		}
 	}
 
