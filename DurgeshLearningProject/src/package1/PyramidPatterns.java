9386f55e7e18517b4b54537481981ade40579b8a
Durgesh <Durgesh@192.168.1.38>
2024-10-19 17:22:19 +0530

    Center Aligned Star Pyramid Pattern

--- DurgeshLearningProject/src/package1/PyramidPatterns.java
+++ DurgeshLearningProject/src/package1/PyramidPatterns.java
@@ -5,17 +5,40 @@ public class PyramidPatterns {
 	public static void main(String[] args) {
 		
 		reversepyramid();
+		StartCenterPyramid();
 	}
 	
 	public static void reversepyramid()
 	{
 		for(int i=0;i<10;i++)
 		{
-			System.out.println();
 			for(int j=i;j<10;j++)
 			{
 				System.out.print("*");
 			}
+			System.out.println();
+		}
+	}
+	
+	public static void StartCenterPyramid()
+	{
+		int p=5;
+		int m=1;
+		for(int o=0;o<6;o++)
+		{
+			for(int l=0;l<p;l++)
+			{
+				System.out.print(" ");
+			}
+			p--;
+			
+			for(int n=0;n<m;n++)
+			{
+				System.out.print("*");
+			}
+			m=m+2;
+			
+			System.out.println();
 		}
 	}
 
