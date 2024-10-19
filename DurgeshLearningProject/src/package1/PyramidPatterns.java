05ceac949872d4ba49f8ad966256449fbcde194d
Durgesh <Durgesh@192.168.1.38>
2024-10-19 09:01:17 +0530

    PyramidPatterns

---
+++ DurgeshLearningProject/src/package1/PyramidPatterns.java
@@ -0,0 +1,22 @@
+package package1;
+
+public class PyramidPatterns {
+
+	public static void main(String[] args) {
+		
+		reversepyramid();
+	}
+	
+	public static void reversepyramid()
+	{
+		for(int i=0;i<10;i++)
+		{
+			System.out.println();
+			for(int j=i;j<10;j++)
+			{
+				System.out.print("*");
+			}
+		}
+	}
+
+}
